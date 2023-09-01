3e5e5f8f110ed1968479fba798e24494f7c9f435
mdrafi276 <mdrafi37473@gmail.com>
2023-09-01 06:48:45 -0700

    first commit

--- script.js
+++ script.js
@@ -23,32 +23,63 @@ const handleCategor = async (id) => {
   const data = await res.json();
   const category = data.data;
   // console.log(category);
-  const categoryCardContainer = document.getElementById("category-card");
-  categoryCardContainer.textContent = '';
-  category.forEach((element) => {
-    const div = document.createElement("div");
-    div.classList = "card bg-base-100 shadow-xl rounded-md";
-    // let img = if()
-    div.innerHTML = `
-        <figure class =" h-52  "><img class="h-full w-full" src=${element.thumbnail} /></figure>
-          <div class="p-5">
-            <div class="flex gap-3">
-            <div class="">
-                <img class="rounded-full w-10 h-10" src=${element.authors[0].profile_picture} alt="" />
+  const CardContainerText = document.getElementById("category-card");
+  CardContainerText.textContent = '';
+  if (category.length === 0) {
+    CardContainerText.textContent = '';
+    CardContainerText.classList.remove('grid');
+    const conditionDiv = document.createElement('div');
+    conditionDiv.innerHTML = `
+      <div class="hero mt-28 ">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+    
+    <figure class="flex justify-center  mb-8">
+        <img  src="./Icon.png" alt="">
+    </figure>
+      <h1 class="text-5xl font-bold">Oops!! Sorry, There is no content here</h1>
+      
+    </div>
+  </div>
+</div>
+
+      `
+      CardContainerText.appendChild(conditionDiv);
+
+  }
+  else {
+    CardContainerText.textContent = '';
+    CardContainerText.classList.add('grid');
+
+    category.forEach((ele) => {
+      const div = document.createElement("div");
+      div.classList = "card bg-base-100 shadow-xl rounded-md";
+      // let img = if()
+      div.innerHTML = `
+        <div><figure class =" h-52  "><img class="h-full w-full" src=${ele.thumbnail} /></figure>
+        <div>
+        <span></span>
+        </div>
+        </div>          
+            <div class="p-5">
+              <div class="flex gap-3">
+              <div class="">
+                  <img class="rounded-full w-10 h-10" src=${ele.authors[0].profile_picture} alt="" />
+              </div>
+              <div class="">
+                  <h2 class="font-semibold">
+                  ${ele.title}
+                  </h2>
+                  <h2 class="text-[#171717B2] font-medium mt-2">${ele.authors[0].profile_name} <span>${ele.authors[0].verified ? `<span><img class=" inline-block" src="./fi_10629607.jpg" alt=""></span> ` : ''}</span></h2>
+                  <p class="text-[#171717B2] font-medium">${ele.others.views}</p>   
+              </div>
+              </div>
             </div>
-            <div class="">
-                <h2 class="font-semibold">
-                ${element.title}
-                </h2>
-                <h2 class="text-[#171717B2] font-medium mt-2">${element.authors[0].profile_name} <span>${element.authors[0].verified ? `<span><img class=" inline-block" src="./fi_10629607.jpg" alt=""></span> `: ''}</span></h2>
-                <p class="text-[#171717B2] font-medium">${element.others.views}</p>   
-            </div>
-            </div>
-          </div>
-        `;
-    categoryCardContainer.appendChild(div);
-  });
-  
+          `;
+      CardContainerText.appendChild(div);
+    });
+  }
+
 };
 
 
